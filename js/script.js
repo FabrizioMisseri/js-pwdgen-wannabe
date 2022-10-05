385711dbe9ee955c98f8a7f878d65262f3076797
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-05 15:09:04 +0200

    added input request

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
+// RACCOGLI DATI INPUT
+    // ins nome
+nome = prompt("inserisci il tuo nome");
+console.log(nome, typeof(nome));
+    // ins cognome
+cognome = prompt("inserisci il tuo cognome");
+console.log(cognome, typeof(cognome));
+    // ins colore
+colore = prompt("inserisci il tuo colore preferito");
+console.log(colore, typeof(colore));
 
-alert("dfssg");
+
