afbafe0eaf1545d4e7ca2ee0fe05efcb9e9d9c7d
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-05 15:47:01 +0200

    added prompt-loop

--- js/script.js
+++ js/script.js
@@ -2,12 +2,21 @@
     // ins nome
 let nome = prompt("inserisci il tuo nome");
 console.log(nome, typeof(nome));
+    while (nome == "") {
+        nome = prompt("inserisci il tuo nome");
+    }
     // ins cognome
 let cognome = prompt("inserisci il tuo cognome");
 console.log(cognome, typeof(cognome));
+while (cognome == "") {
+    cognome = prompt("inserisci il tuo cognome");
+}
     // ins colore
 let colore = prompt("inserisci il tuo colore preferito");
 console.log(colore, typeof(colore));
+while (colore == "") {
+    colore = prompt("inserisci il tuo colore");
+}
 
 
 // ELABORAZIONE DATI
